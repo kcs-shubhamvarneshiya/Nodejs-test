66efd6ff00a11b0405d4d3cdac7a4c828d5cb2e8
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2022-11-01 11:18:22 +0530

    test call stack machanism

--- WeatherApp/test.js
+++ WeatherApp/test.js
@@ -1,22 +1,42 @@
-const axios = require('axios');
-require('colors');
+// const axios = require('axios');
+// require('colors');
 
-const params = {
-    access_key: '5bd90457ca56db496b7956d15cefd7a6',
-    query: 'Ahmedabad'
+// const params = {
+//     access_key: '5bd90457ca56db496b7956d15cefd7a6',
+//     query: 'Ahmedabad'
+// }
+
+// setInterval(() => {
+//     axios.get('http://api.weatherstack.com/current', { params })
+//         .then(response => {
+//             const apiResponse = response.data;
+//             console.log(`Current temperature in 
+//                         ${apiResponse.location.name} ${apiResponse.location.region} 
+//                         is 
+//                         ${apiResponse.current.temperature}℃  
+//                         ${apiResponse.current.weather_descriptions[0]}
+//                         at ${apiResponse.current.observation_time}`.yellow.bold);
+//         }).catch(error => {
+//             console.log(error.message.red.bold);
+//         });
+// }, 5000);
+
+function myfuncOne()
+{
+    console.log('function One Called !!');
+    myfuncTwo();
+    return
 }
 
-setInterval(() => {
-    axios.get('http://api.weatherstack.com/current', { params })
-        .then(response => {
-            const apiResponse = response.data;
-            console.log(`Current temperature in 
-                        ${apiResponse.location.name} ${apiResponse.location.region} 
-                        is 
-                        ${apiResponse.current.temperature}℃  
-                        ${apiResponse.current.weather_descriptions[0]}
-                        at ${apiResponse.current.observation_time}`.yellow.bold);
-        }).catch(error => {
-            console.log(error.message.red.bold);
-        });
-}, 5000);
+function myfuncTwo()
+{
+   
+    setTimeout(()=>{
+        console.log('function Two Called !');
+        //myfuncOne();
+    },5000);
+    console.log('Again Function Two called!')
+    return
+}
+
+myfuncOne();
