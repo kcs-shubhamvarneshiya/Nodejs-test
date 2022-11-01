56e2e57ce581ddbae34d2ffe9aad5fb40072718f
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2022-11-01 17:34:53 +0530

    add weather API

--- WeatherApp/test.js
+++ WeatherApp/test.js
@@ -1,5 +1,5 @@
 // const axios = require('axios');
-// require('colors');
+require('colors');
 
 // const params = {
 //     access_key: '5bd90457ca56db496b7956d15cefd7a6',
@@ -21,22 +21,49 @@
 //         });
 // }, 5000);
 
-function myfuncOne()
-{
-    console.log('function One Called !!');
-    myfuncTwo();
-    return
+// function myfuncOne()
+// {
+//     console.log('function One Called !!');
+//     myfuncTwo();
+//     return
+// }
+
+// function myfuncTwo()
+// {
+
+//     setTimeout(()=>{
+//         console.log('function Two Called !');
+//         //myfuncOne();
+//     },5000);
+//     console.log('Again Function Two called!')
+//     return
+// }
+
+// myfuncOne();
+
+//===================================================
+const request = require('request');
+
+// const parameters = {
+//     method: 'GET',
+//     url: 'https://open-weather13.p.rapidapi.com/city/Ahmedabad',
+//     headers: {
+//         'X-RapidAPI-Key': 'b13f669373msh837a90750215d5cp1e104ejsna8455c0b2a7d',
+//         'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+//     }
+// };
+
+const parameters = {
+    access_key : '5bd90457ca56db496b7956d15cefd7a6',
+    query : 'Ahmedabad'
 }
 
-function myfuncTwo()
-{
-   
-    setTimeout(()=>{
-        console.log('function Two Called !');
-        //myfuncOne();
-    },5000);
-    console.log('Again Function Two called!')
-    return
-}
-
-myfuncOne();
+const url = `http://api.weatherstack.com/current?access_key=5bd90457ca56db496b7956d15cefd7a6&query=Ahmedabad`;
+request({ uri: url, json: true }, function (error, response) {
+    if (error) {
+        console.log(error.message);
+    }
+    else {
+        console.log(response.body);
+    }
+});
