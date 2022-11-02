8b4110a4167587b70a09420cbedb99d7eb0fe91d
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2022-11-02 12:27:49 +0530

    add appropriate message based on weather code

--- WeatherApp/test.js
+++ WeatherApp/test.js
@@ -52,18 +52,46 @@ const request = require('request');
 //         'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
 //     }
 // };
+const access_key = 'c592cc3c9c5cb7e8cb953ebb6d85168f'
+const location = 'morbi'
 
-const parameters = {
-    access_key : '5bd90457ca56db496b7956d15cefd7a6',
-    query : 'Ahmedabad'
-}
+const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}`;
 
-const url = `http://api.weatherstack.com/current?access_key=5bd90457ca56db496b7956d15cefd7a6&query=Ahmedabad`;
 request({ uri: url, json: true }, function (error, response) {
     if (error) {
         console.log(error.message);
     }
     else {
-        console.log(response.body);
+        var msg;
+        var Api = response.body;
+
+        switch (Api.current.weather_code) {
+            case 101:
+                msg = 'look at this , clean sky !'.blue
+                break;
+            case 104:
+                msg = 'See cloudy weather , it seems like rain will come'.blue
+                break;
+            case 105:
+                msg = 'its Rainy day, i need to order Rain coat'.blue
+                break;
+            case 107:
+                msg = 'Look at here , its snowfoll how beautifull it is !!!'.blue
+                break;
+            case 113:
+                msg = 'its freezing rain , i need to order sweater'.blue
+                break;
+            case 114:
+                msg = 'its Thunderstrom !! , please stay at home'.red
+                break;
+            default:
+                msg = 'sorry i dont have proper message for this weather code'.red
+                break;
+        }
+        console.log('Hy bro !, Currently temperature at '.yellow
+            + Api.location.name + ' is '.yellow + Api.current.temperature +
+            ' C ' + msg);
+
+
     }
 });
