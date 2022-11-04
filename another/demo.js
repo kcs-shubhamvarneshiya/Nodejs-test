2c8db862880272082acdeb8c12aeab53519b40e2
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2022-11-04 13:59:52 +0530

    test callback with third party api

--- another/demo.js
+++ another/demo.js
@@ -2,6 +2,8 @@
 //     console.log('Two seconds hold')
 // },2000)
 
+
+
 // const names = ['Andrew','symonds','shubham']
 
 // const shortName = names.filter((name)=>{
@@ -40,11 +42,73 @@
 
 // })
 
-const minFunc = (arg1,arg2,minOfTwo)=>{
-    const minus = arg1-arg2;
-    minOfTwo(minus);
-}
+// const minFunc = (arg1,arg2,minOfTwo)=>{
+//     const minus = arg1-arg2;
+//     minOfTwo(minus);
+// }
 
-minFunc(5,6,(result)=>{
-    console.log(result)
-})
+// minFunc(5,6,(result)=>{
+//     console.log(result)
+// })
+const request = require("request");
+require('colors');
+
+const Getlocation = (url, data) => {
+
+    const param = data
+    request({ uri: url, json: true }, (error, response) => {
+        if (error) {
+
+            if (error.code == 'ENOTFOUND') {
+                param((error, { errorMsg: 'Internet_Connection' }))
+            }
+            else {
+                param(error.message)
+            }
+        }
+        else {
+
+            const res = response.body;
+
+            if (res.message == 'Not Authorized - Invalid Token') {
+                data((error, { errorMsg: 'Invalid_Token' }))
+            }
+            else if (res.features[0] == null) {
+                data((error, { errorMsg: 'Empty_Fields' }))
+            }
+            else {
+                const details = {
+                    place: res.features[0].place_name,
+                    lattitude: res.features[0].center[0],
+                    longitude: res.features[0].center[1]
+                }
+                param(null, details)
+            }
+
+        }
+    })
+
+}
+const loc = 'Rajkot'
+const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${loc}.json?access_token=pk.eyJ1Ijoic2h1YmhhbXY0NTI0IiwiYSI6ImNsOXpmY2cwcTAwMHUzdXBlY3ppbHd6cXMifQ.PMiIpq8nNL741csoDX4oKg&limit=1`
+
+Getlocation(url, (err, result) => {
+
+    if (err) {
+        if (err.errorMsg == 'Internet_Connection') {
+            console.log('Could not connect !, Please check your internet connection !'.red.bold)
+        }
+        else if (err.errorMsg == 'Empty_Fields') {
+            console.log('Could not find the place name , please check again !!'.red.bold)
+        }
+        else if (err.errorMsg == 'Invalid_Token') {
+            console.log('Invalid Token Please , check your token first !!'.red.bold)
+        }
+        else {
+            console.log('Somethig went wrong !!'.red.bold);
+        }
+    }
+    else {
+        console.log(result)
+    }
+});
